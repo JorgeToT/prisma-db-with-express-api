957d26e4ab6df395f45482a70d63702329b12c7b
Jorge Aguilar <jorge.agvel@gmail.com>
2022-05-06 21:48:06 -0500

    Agregando tutorial

---
+++ prisma/migrations/20220507002750_init/migration.sql
@@ -0,0 +1,13 @@
+-- CreateTable
+CREATE TABLE "Explorer" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "username" VARCHAR(255) NOT NULL,
+    "mission" VARCHAR(255) NOT NULL,
+    "azureCertification" BOOLEAN NOT NULL DEFAULT false,
+    "dateCreated" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "lastUpdated" TIMESTAMP(3) NOT NULL
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Explorer_name_key" ON "Explorer"("name");
---
+++ prisma/migrations/20220507023739_init/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "Explorer" ADD CONSTRAINT "Explorer_pkey" PRIMARY KEY ("id");
